e89b1addf9eb4cd7c03bfd7d0bf14c5f0c83a185
piyatida646kp <59428776+piyatida646@users.noreply.github.com>
2021-11-05 17:38:23 +0700

    Create SingletonClient.java

---
+++ src/SingletonClient.java
@@ -0,0 +1,11 @@
+
+public class SingletonClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Singleton singleton = Singleton.getInstance();
+		System.out.print(singleton.getDescription());
+		
+	}
+
+}
